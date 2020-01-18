bb111a9140b7bcc7a0c6f27d2a4dfe273ce6d663
Sylvain Mougenot <sylvain.mougenot@gmail.com>
2020-01-18 23:34:16 +0100

    Change formatter

--- src/esp32_sensor_display.ino
+++ src/esp32_sensor_display.ino
@@ -1,17 +1,20 @@
-#include <TFT_eSPI.h>
-#include <SPI.h>
-#include "WiFi.h"
-#include <Wire.h>
 #include <Button2.h>
-#include "esp_adc_cal.h"
-#include "bmp.h"
-#include <WiFiManager.h>
 #include <EEPROM.h>
+#include <SPI.h>
+#include <TFT_eSPI.h>
+#include <WiFiManager.h>
+#include <Wire.h>
+#include "WiFi.h"
+#include "bmp.h"
+#include "esp_adc_cal.h"
 
 // I2C sensors
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+// send MQTT messages
+#include <PubSubClient.h>
 
 #ifndef TFT_DISPOFF
 #define TFT_DISPOFF 0x28
@@ -30,9 +33,8 @@
 #define LOOP_WAIT 6 * 1000
 
 // MQTT Settings
-#define EEPROM_SALT 311276 // to check version
-typedef struct
-{
+#define EEPROM_SALT 311276  // to check version
+typedef struct {
   char mqtt_server[120] = "";
   char mqtt_port[6] = "1883";
   char mqtt_user[120] = "";
@@ -46,10 +48,10 @@ WMSettings settings;
 // #define IC_CLK  22
 // #define IC_DATA 21
 // In case no device is responding, initiate a I2C bus scan
-byte i2c_scan_start_address = 8; // lower addresses are reserved to prevent conflicts with other protocols
-byte i2c_scan_end_address = 119; // higher addresses unlock other modes, like 10-bit addressing
+byte i2c_scan_start_address = 8;  // lower addresses are reserved to prevent conflicts with other protocols
+byte i2c_scan_end_address = 119;  // higher addresses unlock other modes, like 10-bit addressing
 
-TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+TFT_eSPI tft = TFT_eSPI(135, 240);  // Invoke custom library
 Button2 btn1(BUTTON_1);
 Button2 btn2(BUTTON_2);
 
@@ -75,19 +77,24 @@ long lastCheck = 0;
 
 char chipId[23];
 
-//! Long time delay, it is recommended to use shallow sleep, which can effectively reduce the current consumption
-void espDelay(int ms)
-{
+// const String topicCmd    = "esp/" + String(ESP.getChipId()) + "/cmd";
+// const String topicStatus = "esp/" + String(ESP.getChipId()) + "/status";
+// const String clientId = "ESP8266Client" + String(ESP.getChipId()) ;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+//! Long time delay, it is recommended to use shallow sleep, which can
+//! effectively reduce the current consumption
+void espDelay(int ms) {
   esp_sleep_enable_timer_wakeup(ms * 1000);
   esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
   esp_light_sleep_start();
 }
 
-void showVoltage()
-{
+void showVoltage() {
   static uint64_t timeStamp = 0;
-  if (millis() - timeStamp > 1000)
-  {
+  if (millis() - timeStamp > 1000) {
     timeStamp = millis();
     readVoltage();
     String voltage = "Voltage :" + String(battery_voltage) + "V";
@@ -98,8 +105,7 @@ void showVoltage()
   }
 }
 
-void button_init()
-{
+void button_init() {
   btn1.setPressedHandler([](Button2 &b) {
     Serial.println("Detect Voltage..");
     btn1Clicked != btn1Clicked;
@@ -114,33 +120,26 @@ void button_init()
   });
 }
 
-void button_loop()
-{
+void button_loop() {
   btn1.loop();
   btn2.loop();
 }
 
-void configModeCallback(WiFiManager *myWiFiManager)
-{
+void configModeCallback(WiFiManager *myWiFiManager) {
   Serial.println("Entered config mode");
   Serial.println(WiFi.softAPIP());
-  //if you used auto generated SSID, print it
+  // if you used auto generated SSID, print it
   Serial.println(myWiFiManager->getConfigPortalSSID());
   displayWifiManager(myWiFiManager->getConfigPortalSSID(), WiFi.softAPIP());
 }
 
-void saveWifiCallback()
-{
+void saveWifiCallback() {
   Serial.println("[CALLBACK] save Wifi callback fired");
 }
 
-void saveParamCallback()
-{
-  Serial.println("[CALLBACK] param callback fired");
-}
+void saveParamCallback() { Serial.println("[CALLBACK] param callback fired"); }
 
-void configLoad()
-{
+void configLoad() {
   Serial.println("Loading config");
 
   EEPROM.begin(512);
@@ -150,18 +149,14 @@ void configLoad()
   Serial.println("Config loaded");
 }
 
-void configSave()
-{
+void configSave() {
   Serial.println("Saving config");
 
   EEPROM.begin(512);
   EEPROM.put(0, settings);
-  if (EEPROM.commit())
-  {
+  if (EEPROM.commit()) {
     Serial.println("Config saved");
-  }
-  else
-  {
+  } else {
     Serial.println("EEPROM error");
   }
   EEPROM.end();
@@ -173,13 +168,12 @@ void resetSettings() {
   configSave();
 }
 
-void initWifiManager()
-{
+void initWifiManager() {
   Serial.println("initWifiManager");
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
+  // WiFiManager
+  // Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
-  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  // set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
   wifiManager.setAPCallback(configModeCallback);
   wifiManager.setSaveConfigCallback(saveWifiCallback);
   wifiManager.setSaveParamsCallback(saveParamCallback);
@@ -195,14 +189,14 @@ void initWifiManager()
   wifiManager.addParameter(&custom_mqtt_user);
   wifiManager.addParameter(&custom_mqtt_password);
 
-  //set static ip
+  // set static ip
   // wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0)); // set static ip,gw,sn
   // wifiManager.setShowStaticFields(true); // force show static ip fields
   // wifiManager.setShowDnsFields(true);    // force show dns field always
 
-  wifiManager.setConfigPortalTimeout(120); // auto close configportal after n seconds
+  wifiManager.setConfigPortalTimeout(120);  // auto close configportal after n seconds
 
-  wifiManager.setCaptivePortalEnable(false); // disable captive portal redirection
+  wifiManager.setCaptivePortalEnable(false);  // disable captive portal redirection
   // wifiManager.setAPClientCheck(true); // avoid timeout if client connected to softap
 
   // wifi scan settings
@@ -217,11 +211,10 @@ void initWifiManager()
   String ssid = "ESP_autoconnect_" + String(chipId);
   String pwd = "password";
 
-  if (!wifiManager.autoConnect(ssid.c_str(), pwd.c_str()))
-  {
+  if (!wifiManager.autoConnect(ssid.c_str(), pwd.c_str())) {
     Serial.println("failed to connect and hit timeout");
     espDelay(3000);
-    //reset and try again, or maybe put it to deep sleep
+    // reset and try again, or maybe put it to deep sleep
     ESP.restart();
   }
 
@@ -235,14 +228,13 @@ void initWifiManager()
   configSave();
 }
 
-void initChipId()
-{
-  uint64_t chipMac = ESP.getEfuseMac(); //The chip ID is essentially its MAC address(length: 6 bytes).
-  snprintf(chipId, 23, "%04X%08X", (uint16_t)(chipMac >> 32), (uint32_t)chipMac);
+void initChipId() {
+  uint64_t chipMac = ESP.getEfuseMac();  // The chip ID is essentially its MAC address(length: 6 bytes).
+  snprintf(chipId, 23, "%04X%08X", (uint16_t)(chipMac >> 32),
+           (uint32_t)chipMac);
 }
 
-void initTft()
-{
+void initTft() {
   tft.init();
   tft.setRotation(1);
   Serial.println("tft to black");
@@ -255,10 +247,10 @@ void initTft()
   tft.setTextSize(1);
 
   Serial.println("tft backlight");
-  if (TFT_BL > 0)
-  {                                         // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-    pinMode(TFT_BL, OUTPUT);                // Set backlight pin to output mode
-    digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+  if (TFT_BL > 0) {           // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+    pinMode(TFT_BL, OUTPUT);  // Set backlight pin to output mode
+    digitalWrite(TFT_BL,
+                 TFT_BACKLIGHT_ON);  // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
   }
 
   Serial.println("tft swap bytes");
@@ -270,8 +262,7 @@ void initTft()
   Serial.println("tft colors");
   tft.setRotation(0);
   int i = 5;
-  while (i--)
-  {
+  while (i--) {
     tft.fillScreen(TFT_RED);
     espDelay(250);
     tft.fillScreen(TFT_BLUE);
@@ -282,22 +273,19 @@ void initTft()
   tft.fillScreen(TFT_BLACK);
 }
 
-void initVref()
-{
+void initVref() {
   esp_adc_cal_characteristics_t adc_chars;
-  esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
-  //Check type of calibration value used to characterize ADC
-  if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF)
-  {
+  esp_adc_cal_value_t val_type = esp_adc_cal_characterize(
+      (adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6,
+      (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
+  // Check type of calibration value used to characterize ADC
+  if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
     Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
     vref = adc_chars.vref;
-  }
-  else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP)
-  {
-    Serial.printf("Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
-  }
-  else
-  {
+  } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+    Serial.printf("Two Point --> coeff_a:%umV coeff_b:%umV\n",
+                  adc_chars.coeff_a, adc_chars.coeff_b);
+  } else {
     Serial.println("Default Vref: 1100mV");
   }
 }
@@ -306,11 +294,9 @@ void initVref()
 // On each address, call the callback function with the address and result.
 // If result==0, address has a device
 void scanI2CBus(byte from_addr, byte to_addr,
-                void (*callback)(byte address, byte result))
-{
+                void (*callback)(byte address, byte result)) {
   byte rc;
-  for (byte addr = from_addr; addr <= to_addr; addr++)
-  {
+  for (byte addr = from_addr; addr <= to_addr; addr++) {
     Wire.beginTransmission(addr);
     rc = Wire.endTransmission();
     callback(addr, rc);
@@ -319,30 +305,29 @@ void scanI2CBus(byte from_addr, byte to_addr,
 
 // Called when address is scanned scanI2CBus()
 // If result==0, address has a device
-void scanFunc(byte addr, byte result)
-{
+void scanFunc(byte addr, byte result) {
   Serial.print("addr: ");
   Serial.print(addr, HEX);
   Serial.print((result == 0) ? " found!" : "       ");
   Serial.print((addr % 4) ? "\t" : "\n");
 }
 
-void initBME()
-{
-  if (!bme.begin(0x76, &Wire))
-  {
-    Serial.println(F("Could not find a valid BME280 sensor, check wiring, address, sensor ID!"));
+void initBME() {
+  if (!bme.begin(0x76, &Wire)) {
+    Serial.println(
+        F("Could not find a valid BME280 sensor, check wiring, address, sensor "
+          "ID!"));
     Serial.print(F("SensorID was: 0x"));
     Serial.println(bme.sensorID(), 16);
-    Serial.print(F("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n"));
+    Serial.print(
+        F("        ID of 0xFF probably means a bad address, a BMP 180 or BMP "
+          "085\n"));
     Serial.print(F("   ID of 0x56-0x58 represents a BMP 280,\n"));
     Serial.print(F("        ID of 0x60 represents a BME 280.\n"));
     Serial.print(F("        ID of 0x61 represents a BME 680.\n"));
     Serial.println(F("Will process to I2C scan"));
     bme_available = false;
-  }
-  else
-  {
+  } else {
     bme_available = true;
   }
 
@@ -351,8 +336,12 @@ void initBME()
   bme_humidity->printSensorDetails();
 }
 
-void setup()
-{
+void initMqtt() {
+  // client.setClient("ESP32Client" + String(chipId));
+  // client.setServer(settings.mqtt_server, String(settings.mqtt_port).toInt());
+}
+
+void setup() {
   Serial.begin(115200);
   Serial.println("Start");
 
@@ -377,27 +366,22 @@ void setup()
 
   Serial.println("WifiManager init");
   initWifiManager();
+
+  // Init MQTT client
+  initMqtt();
 }
 
-void loop()
-{
+void loop() {
   button_loop();
   long now = millis();
-  if (now - lastCheck > LOOP_WAIT)
-  {
+  if (now - lastCheck > LOOP_WAIT) {
     lastCheck = now;
-    if (btn1Clicked)
-    {
+    if (btn1Clicked) {
       showVoltage();
-    }
-    else
-    {
-      if (!bme_available)
-      {
+    } else {
+      if (!bme_available) {
         scanI2CBus(i2c_scan_start_address, i2c_scan_end_address, scanFunc);
-      }
-      else
-      {
+      } else {
         readSensor();
         readVoltage();
         displaySensor(temperature, humidity, battery_voltage);
@@ -409,14 +393,12 @@ void loop()
   }
 }
 
-void readVoltage()
-{
+void readVoltage() {
   uint16_t v = analogRead(ADC_PIN);
   battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
 }
 
-void readSensor()
-{
+void readSensor() {
   sensors_event_t temp_event, pressure_event, humidity_event;
   bme_temp->getEvent(&temp_event);
   bme_pressure->getEvent(&pressure_event);
@@ -441,8 +423,7 @@ void readSensor()
   Serial.println();
 }
 
-void displayWifiManager(String wifiAP, IPAddress configAddress)
-{
+void displayWifiManager(String wifiAP, IPAddress configAddress) {
   tft.fillScreen(TFT_BLACK);
   tft.setTextDatum(TL_DATUM);
   tft.setRotation(1);
@@ -450,15 +431,11 @@ void displayWifiManager(String wifiAP, IPAddress configAddress)
   tft.setTextSize(2);
   tft.setCursor(0, 0);
 
-  sprintf(buff,
-          "%s\n%s",
-          wifiAP.c_str(),
-          configAddress.toString().c_str());
+  sprintf(buff, "%s\n%s", wifiAP.c_str(), configAddress.toString().c_str());
   tft.println(buff);
 }
 
-void displaySensor(float aTemperature, float aHumidity, float aVoltage)
-{
+void displaySensor(float aTemperature, float aHumidity, float aVoltage) {
   tft.fillScreen(TFT_BLACK);
   tft.setTextDatum(TL_DATUM);
   tft.setRotation(0);
@@ -466,10 +443,6 @@ void displaySensor(float aTemperature, float aHumidity, float aVoltage)
   tft.setTextSize(3);
   tft.setCursor(0, 0);
 
-  sprintf(buff,
-          "%.2f C\n%.2f %%\n %.2f V",
-          aTemperature,
-          aHumidity,
-          aVoltage);
+  sprintf(buff, "%.2f C\n%.2f %%\n %.2f V", aTemperature, aHumidity, aVoltage);
   tft.println(buff);
 }
